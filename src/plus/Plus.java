1bb9c1838538ea282038a74121732edb2f2a4041
好吃叔 <724193667@qq.com>
2017-10-30 22:05:43 +0800

    反复问一个加法题

---
+++ src/plus/Plus.java
@@ -0,0 +1,54 @@
+package plus;
+
+import java.util.ArrayList;
+
+public class Plus {
+	private int p1, p2;
+	private ArrayList<Integer> answer = new ArrayList<>();
+	
+	public Plus(int p1, int p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+	
+	public int getP1() {
+		return this.p1;
+	}
+	
+	public int getP2() {
+		return this.p2;
+	}
+	
+	public void setP1P2(int p1, int p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+	
+	public void key(int a) {
+		answer.add((Integer) a);
+	}
+	
+	public void result() {
+		if(answer.get(answer.size() - 1) != (Integer)(this.p1 + this.p2)) {
+			if(answer.size() >= 2) {
+				for(int i = 0; i <= answer.size() - 2; i++) {
+					if(answer.get(i) == answer.get(answer.size() - 1)) {
+						System.out.println("你写过这个答案了");
+						break;
+					}
+					else {
+						System.out.println("错误答案！");
+						break;
+					}
+				}
+			}
+			else {
+				System.out.println("错误答案！");
+			}				
+		}
+		else {
+			System.out.println("答案正确！");	
+			System.exit(1);
+		}
+	}
+}
---
+++ src/plus/Test.java
@@ -0,0 +1,19 @@
+package plus;
+
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		Plus a = new Plus(5, 9);
+		Scanner input = new Scanner(System.in);
+		
+		do {
+			System.out.println(a.getP1() + "+" + a.getP2() + "= ?");
+			int key = input.nextInt();
+			a.key(key);
+			a.result();			
+		}while(true);
+		
+	}
+	
+}
